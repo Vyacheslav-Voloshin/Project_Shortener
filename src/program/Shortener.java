e4c4ea142c25049b29e7bbbd21e6224b15a248a9
Vyacheslav-Voloshin <oks.volosh1na1981@gmail.com>
2024-11-05 20:35:06 +0200

    created necessary classes

---
+++ src/program/Shortener.java
@@ -0,0 +1,15 @@
+package program;
+
+public class Shortener {
+
+
+    // метод повертає ідентифікатор id для заданої строки
+    public Long getId(String string){
+        return null;
+    }
+
+    // метод буде повертати строку для заданого ідентифікатора або null, якщо передано невірний ідентифікатор.
+    public String getString(Long id){
+        return "";
+    }
+}
